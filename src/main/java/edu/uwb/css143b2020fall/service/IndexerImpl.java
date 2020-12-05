bf5f9ccb8cf1a55230bd9d607a7bf36f3d63cc73
TalonM <talonm@uw.edu>
2020-12-05 15:11:20 -0800

    my indexer is a little more fleshed out but not working quite yet

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -9,18 +9,34 @@ public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
         // add your code
-        for (int docnumber = 0; docnumber < docs.size(); docnumber++) {
+        for (int docnumber = 0; docnumber < docs.size(); docnumber++) { //Check each document
 
             String[] words = docs.get(docnumber).trim().split("\\s+");
 
-            for (int i = 0; i < words.length; i++) {
-                List<List<Integer>> document = new ArrayList<>(docs.size());
-                List<Integer> location = new ArrayList<>();
+            for (int pos = 0; pos < words.length; pos++) { //Check each word in each document
 
-                location.add(i);
-                document.add(docnumber, location);
-                indexes.put(words[i], document);
+                List<Integer> locations = new ArrayList<>();
+
+                locations.add(pos);
+
+                for (int pos2 = pos + 1; pos2 < words.length; pos2++) { //Check the rest of the words in this document to see if this word shows up multiple times
+                    if (words[pos2].equals(words[pos]))
+                        locations.add(pos2);
+                }
+
+                if (indexes.containsKey(words[pos])) { //Check to see if the index for this word has already been created
+
+                    List<List<Integer>> documents = new ArrayList<>(docs.size()); //The documents each word appears in
+                    documents.add(docnumber, locations);
+                    indexes.put(words[pos], documents);
+
+                } else { //If the index has already been created
+                    indexes.get(words[pos]).add(docnumber, locations);
+                }
+
+
             }
+
         }
         return indexes;
     }
