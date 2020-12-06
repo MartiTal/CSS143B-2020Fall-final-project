ba0eb86de1da468c20c193593c99d27534cec538
TalonM <talonm@uw.edu>
2020-12-05 16:27:59 -0800

    searcher started

--- src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
@@ -16,6 +16,17 @@ public class SearcherImpl implements Searcher {
     public List<Integer> search(String keyPhrase, Map<String, List<List<Integer>>> index) {
         List<Integer> result = new ArrayList<>();
         // add your code
+        String[] searchedWords = keyPhrase.trim().split(" ");
+        if (searchedWords.length == 1) {
+            if (searchedWords[0].length() != 0) { //Ignore this part in the case of a search phrase being an empty String (test case)
+                for (int i = 0; i < index.get(searchedWords[0]).size(); i++) {
+                    if ( !(index.get(searchedWords[0]).get(i).isEmpty()) )
+                        result.add(i);
+                }
+            }
+            return result;
+        }
+
         return result;
     }
 }
