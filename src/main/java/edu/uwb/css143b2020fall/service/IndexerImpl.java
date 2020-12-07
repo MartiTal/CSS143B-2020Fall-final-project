9340dda8a8988420471e847cb63d045b305e54cc
TalonM <talonm@uw.edu>
2020-12-06 21:19:50 -0800

    reverting a small change in the IndexerImpl to see if this fixes anything

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -2,7 +2,10 @@ package edu.uwb.css143b2020fall.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class IndexerImpl implements Indexer {
